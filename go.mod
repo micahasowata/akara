b94b926ac2c55206f140d214c08c7a1ca16e3b88
Micah Asowata <andrewgary963@gmail.com>
2025-05-07 01:55:06 +0100

    add layouts

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module micahasowata.com/akara
 
 go 1.24.1
 
-require github.com/yuin/goldmark v1.7.11 // indirect
+require (
+	github.com/tdewolff/minify/v2 v2.23.3
+	github.com/yuin/goldmark v1.7.11
+)
+
+require github.com/tdewolff/parse/v2 v2.7.23 // indirect
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	err := buildFile("/home/micah/projects/akaratest/content/posts/post-10.md", "/home/micah/projects/akaratest/target")
+	err := build("/home/micah/projects/akaratest/layouts/base.html", "/home/micah/projects/akaratest/content/posts/post-2.md", "/home/micah/projects/akaratest/target")
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
