f37d5ef2900ca78b46ea40a7033689631c691c3f
Micah Asowata <andrewgary963@gmail.com>
2025-05-11 16:18:09 +0100

    convert md to html

--- main.go
+++ main.go
@@ -2,51 +2,15 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/tdewolff/minify/v2"
-	"github.com/tdewolff/minify/v2/css"
-	"micahasowata.com/akara/build"
+	"micahasowata.com/akara/content"
 )
 
 func main() {
-	html, err := build.ConvertMdToHTML("/home/micah/projects/akaratest/content/posts/post-10.md")
+	h, err := content.ConvertToHTML("/home/micah/projects/akaratest/content/posts/post-1.md")
 	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
+		panic(err)
 	}
 
-	html, err = build.ResolveToLayout("/home/micah/projects/akaratest/layouts/base.html", html)
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	html, err = build.RemoveLocalStyleLinks(html)
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	localStyles := build.ReadInFileStyles(html)
-	extStyles, err := build.ReadSectionStyles("/home/micah/projects/akaratest/css/posts.css")
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	ogCSS := append(extStyles, localStyles...)
-
-	m := minify.New()
-	m.AddFunc("text/css", css.Minify)
-	fmt.Println(string(ogCSS))
-
-	mCSS, err := m.Bytes("text/css", ogCSS)
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	fmt.Println(string(mCSS))
+	fmt.Println(string(h))
 }
